ee78382ac554ea82a02c35e7529f210f22cf3fa8
l-arthu <arthur05luis@gmail.com>
2023-05-19 08:52:37 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function ativarNome(){
+    let nome = 'Sávio';
+    console.log(nome);
+    document.querySelector(".modulo").innerHTML="<p style= color:red >Sávio</p>"
+}
+
+
+function so(){
+    let a = 5;
+    let b = 10;
+
+
+    let c = a+b;
+
+    console.log(c);
+    document.querySelector(".modu").innerHTML="<p style= color:red >15</p>"
+}
+
